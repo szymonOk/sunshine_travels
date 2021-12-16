5e0ceb2927529ef3e97eabeb2b5240d7300423e6
Szymon Okulowski <szymon.okulowski@gmail.com>
2021-12-16 21:52:40 +0100

    add javascript scripts

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 const nav = document.querySelector('.nav__container')
 const eachNavItem = document.querySelectorAll('.nav__item')
 const btn = document.querySelector('.nav__btn')
+const footerYear = document.querySelector('.footer__year')
+const navBars = document.querySelector('.nav__bars')
+const allSections = document.querySelectorAll('.section')
 
+// BLACK NAVIGATION
+const handleObserver = () => {
+	const currentSection = window.scrollY
+
+	allSections.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBars.classList.add('black-bars')
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBars.classList.remove('black-bars')
+		}
+	})
+}
+
+// NAVIGATION SLIDE
 const handleNav = () => {
 	nav.classList.toggle('nav__container--active')
+	navBars.classList.remove('black-bars')
 
 	eachNavItem.forEach(item => {
 		item.addEventListener('click', () => {
@@ -14,6 +32,7 @@ const handleNav = () => {
 	handleNavItemsAnimation()
 }
 
+// NAVIGATION ANIMATION
 const handleNavItemsAnimation = () => {
 	let delayTime = 0
 
@@ -24,4 +43,12 @@ const handleNavItemsAnimation = () => {
 	})
 }
 
+// YEAR
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+handleCurrentYear()
 btn.addEventListener('click', handleNav)
+window.addEventListener('scroll', handleObserver)
